077d14c1ae9f037d55a95cc9f2e7808841172c2b
Adam Landfrasz <adam.landfrasz@gmail.com>
2024-05-08 15:49:35 +0200

    first test case reads credentials and logs in

--- test/pages/swag-labs.ts
+++ test/pages/swag-labs.ts
@@ -1,10 +1,27 @@
+import fs from 'fs/promises';
 import { type Page, type Locator } from '@playwright/test';
 import { BasePage } from './base-page';
 
-export class SwagLabs extends BasePage {
+export class SwagLabsHome extends BasePage {
     private usernameInput: Locator;
+    private passwordInput: Locator;
+    private loginButton: Locator;
     constructor(page: Page) {
-        super(page, 'https://www.saucedemo.com/inventory.html');
+        super(page, 'https://www.saucedemo.com');
         this.usernameInput = this.page.locator('#user-name');
+        this.passwordInput = this.page.locator('#password');
+        this.loginButton = this.page.locator('#login-button');
+    }
+
+    async logInWithCredentials() {
+        const credentials: { username: string; password: string } = JSON.parse(
+            await fs.readFile(
+                `${process.cwd()}/test/resources/credentials.json`,
+                'utf-8',
+            ),
+        );
+        await this.usernameInput.fill(credentials.username);
+        await this.passwordInput.fill(credentials.password);
+        await this.loginButton.click();
     }
 }
--- tests/purchase-process.spec.ts
+++ tests/purchase-process.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
-import { SwagLabs } from '../test/pages/swag-labs';
+import { SwagLabsHome } from '../test/pages/swag-labs';
 
 test.describe('Automated Purchase Process', () => {
     test('log in with credentials and go through check out process', async ({
         page,
     }) => {
-        const swagLabsPage = new SwagLabs(page);
+        const swagLabsPage = new SwagLabsHome(page);
         await swagLabsPage.goto();
         await expect(page).toHaveTitle(/Swag Labs/);
+        await swagLabsPage.logInWithCredentials();
+        await expect(page).toHaveURL(/inventory.html/);
     });
 });
